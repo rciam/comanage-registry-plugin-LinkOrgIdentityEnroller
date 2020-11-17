daf388a1b7bbb6b3c85e8b27808db6a3863e68a6
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2020-11-17 09:41:21 +0200

    Refactored installation scripts

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -1,16 +1,42 @@
 <?php
-class AppSchema extends CakeSchema
-{
-  
-  public function before($event = array())
-  {
+
+class AppSchema extends CakeSchema {
+  public $connection = 'default';
+
+  public function before($event = array()) {
     return true;
   }
-  
-  public function after($event = array())
-  {
+
+  public function after($event = array()) {
+    if(isset($event['create'])) {
+      $sql_cmd = "";
+      switch($event['create']) {
+        case 'cm_link_org_identity_enrollers':
+          $LinkOrgIdentityEnroller = ClassRegistry::init('LinkOrgIdentityEnroller.LinkOrgIdentityEnroller');
+          $LinkOrgIdentityEnroller->useDbConfig = $this->connection;
+          $sql_cmd = "ALTER TABLE ONLY public.cm_link_org_identity_enrollers ADD CONSTRAINT cm_link_org_identity_enrollers_co_id_fkey FOREIGN KEY (co_id) REFERENCES public.cm_cos(id)";
+          // Add the constraints or any other initializations
+          $LinkOrgIdentityEnroller->query($sql_cmd);
+          break;
+        case 'cm_link_org_identity_eofs':
+          $LinkOrgIdentityEof = ClassRegistry::init('LinkOrgIdentityEnroller.LinkOrgIdentityEof');
+          $LinkOrgIdentityEof->useDbConfig = $this->connection;
+          $sql_cmd = "ALTER TABLE ONLY public.cm_link_org_identity_eofs ADD CONSTRAINT cm_link_org_identity_eofs_link_org_identity_enroller_id_fkey FOREIGN KEY (link_org_identity_enroller_id) REFERENCES public.cm_link_org_identity_enrollers(id);";
+          $sql_cmd .= "ALTER TABLE ONLY public.cm_link_org_identity_eofs ADD CONSTRAINT cm_link_org_identity_eofs_co_enrollment_flow_id_fkey FOREIGN KEY (co_enrollment_flow_id) REFERENCES public.cm_co_enrollment_flows(id);";
+          // Add the constraints or any other initializations
+          $LinkOrgIdentityEof->query($sql_cmd);
+          break;
+        case 'cm_link_org_identity_states':
+          $LinkOrgIdentityState = ClassRegistry::init('LinkOrgIdentityEnroller.LinkOrgIdentityState');
+          $LinkOrgIdentityState->useDbConfig = $this->connection;
+          $sql_cmd = "ALTER TABLE ONLY public.cm_link_org_identity_states ADD CONSTRAINT cm_link_org_identity_states_link_org_identity_enroller_id_fkey FOREIGN KEY (link_org_identity_enroller_id) REFERENCES public.cm_link_org_identity_enrollers(id);";
+          // Add the constraints or any other initializations
+          $LinkOrgIdentityState->query($sql_cmd);
+          break;
+      }
+    }
   }
-  
+
   public $link_org_identity_enrollers = array(
     'id' => array('type' => 'integer', 'autoIncrement' => true, 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
     'co_id' => array('type' => 'integer', 'null' => true, 'length' => 10),
@@ -31,7 +57,7 @@ class AppSchema extends CakeSchema
       'PRIMARY' => array('column' => 'id', 'unique' => 1),
     )
   );
-  
+
   public $link_org_identity_states = array(
     'id' => array('type' => 'integer', 'autoIncrement' => true, 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
     'link_org_identity_enroller_id' => array('type' => 'integer', 'null' => true, 'length' => 10),
@@ -46,7 +72,7 @@ class AppSchema extends CakeSchema
       'link_org_identity_states_i1' => array('column' => 'token'),
     )
   );
-  
+
   public $link_org_identity_eofs = array(
     'id' => array('type' => 'integer', 'autoIncrement' => true, 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
     'co_enrollment_flow_id' => array('type' => 'integer', 'null' => false, 'length' => 10),
@@ -62,25 +88,12 @@ class AppSchema extends CakeSchema
 
 
 /**
-CREATE TABLE cm_link_org_identity_eofs(
-  id serial PRIMARY KEY,
-  co_enrollment_flow_id  INTEGER NOT NULL REFERENCES cm_co_enrollment_flows(id) ON DELETE CASCADE,
-  link_org_identity_enroller_id  INTEGER NOT NULL REFERENCES cm_link_org_identity_enrollers(id),
-  created TIMESTAMP NOT NULL,
-  modified TIMESTAMP NOT NULL,
-  deleted BOOLEAN
-);
-
-// Console/cake schema create --file schema.php --path /srv/comanage/registry-current/local/Plugin/LinkOrgIdentityEnroller/Config/Schema
-
-ALTER TABLE ONLY public.cm_link_org_identity_enrollers ADD CONSTRAINT cm_link_org_identity_enrollers_co_id_fkey FOREIGN KEY (co_id) REFERENCES public.cm_cos(id);
-
-ALTER TABLE ONLY public.cm_link_org_identity_states ADD CONSTRAINT cm_link_org_identity_states_link_org_identity_enroller_id_fkey FOREIGN KEY (link_org_identity_enroller_id) REFERENCES public.cm_link_org_identity_enrollers(id);
-
-ALTER TABLE ONLY public.cm_link_org_identity_eofs ADD CONSTRAINT cm_link_org_identity_eofs_link_org_identity_enroller_id_fkey FOREIGN KEY (link_org_identity_enroller_id) REFERENCES public.cm_link_org_identity_enrollers(id);
-ALTER TABLE ONLY public.cm_link_org_identity_eofs ADD CONSTRAINT cm_link_org_identity_eofs_co_enrollment_flow_id_fkey FOREIGN KEY (co_enrollment_flow_id) REFERENCES public.cm_co_enrollment_flows(id);
-
-GRANT SELECT ON TABLE public.cm_link_org_identity_states TO cmregistryuser_proxy;
-GRANT SELECT ON TABLE public.cm_link_org_identity_enrollers TO cmregistryuser_proxy;
-GRANT SELECT ON TABLE public.cm_link_org_identity_eofs TO cmregistryuser_proxy;
+ * // Console/cake schema create --file schema.php --path /srv/comanage/registry-current/local/Plugin/LinkOrgIdentityEnroller/Config/Schema
+ *
+ * ALTER TABLE ONLY public.cm_link_org_identity_enrollers ADD CONSTRAINT cm_link_org_identity_enrollers_co_id_fkey FOREIGN KEY (co_id) REFERENCES public.cm_cos(id);
+ *
+ * ALTER TABLE ONLY public.cm_link_org_identity_states ADD CONSTRAINT cm_link_org_identity_states_link_org_identity_enroller_id_fkey FOREIGN KEY (link_org_identity_enroller_id) REFERENCES public.cm_link_org_identity_enrollers(id);
+ *
+ * ALTER TABLE ONLY public.cm_link_org_identity_eofs ADD CONSTRAINT cm_link_org_identity_eofs_link_org_identity_enroller_id_fkey FOREIGN KEY (link_org_identity_enroller_id) REFERENCES public.cm_link_org_identity_enrollers(id);
+ * ALTER TABLE ONLY public.cm_link_org_identity_eofs ADD CONSTRAINT cm_link_org_identity_eofs_co_enrollment_flow_id_fkey FOREIGN KEY (co_enrollment_flow_id) REFERENCES public.cm_co_enrollment_flows(id);
  */
