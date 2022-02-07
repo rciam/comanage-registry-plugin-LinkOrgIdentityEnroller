61054b7ab028222f89b8d907f5de97e3f5aad9ca
Ioannis Igoumenos <ioigoume@admin.grnet.gr>
2022-02-07 09:53:58 +0200

    Fix broken idphint redirect during implicit linking.Fixed buggy routes configuration.

--- Config/routes.php
+++ Config/routes.php
@@ -5,12 +5,6 @@
  */
 
 Router::mapResources(array(
-  'link_org_identity_eofs'
+  'LinkOrgIdentityEnroller.link_org_identity_eofs'
 ));
-Router::parseExtensions();
 
-/**
- * Load the CakePHP default routes. Remove this if you do not want to use
- * the built-in default routes.
- */
-require CAKE . 'Config' . DS . 'routes.php';
